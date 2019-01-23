b8c46df4472e86defa4088adc41ccc206a2abdd4
Bouurdelle Josselin <j.bourdelle@laposte.net>
2019-01-23 17:40:11 +0100

    Ajout des pages de l'app plus nav entre elles

---
+++ Familink/ContactListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactListTableViewCell.swift
+//  Familink
+//
+//  Created by formation12 on 23/01/2019.
+//  Copyright © 2019 ti.punch. All rights reserved.
+//
+
+import UIKit
+
+class ContactListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
