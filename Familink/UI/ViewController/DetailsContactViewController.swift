8bcc2f879635aef4fdec1305f835847bc477ab38
Clement coxam <clement.coxam@gmail.com>
2019-01-28 16:47:00 +0100

    Edition et Modification sur le Detail Contact
    Regex et alerte sur l'email 
    Regex et alerte sur le téléphone

--- Familink/UI/ViewController/DetailsContactViewController.swift
+++ Familink/UI/ViewController/DetailsContactViewController.swift
@@ -45,8 +45,6 @@ class DetailsContactViewController: UIViewController, UIPickerViewDelegate, UIPi
             }
         }
         
-        
-        
     }
     
     @IBAction func tapToMail(_ sender: UIButton) {
@@ -58,9 +56,17 @@ class DetailsContactViewController: UIViewController, UIPickerViewDelegate, UIPi
     @IBAction func tapToCall(_ sender: UIButton) {
     }
     @IBAction func tapToSave(_ sender: UIButton) {
-        self.saveButton.isHidden = true
-        self.editButton.isHidden = false
         
+        if !isValidEmail(email:self.emailTextInput.text!) && !isValidPhone(phone: self.phoneTextInput.text!){
+            self.saveButton.isHidden = true
+            self.editButton.isHidden = false
+            self.firstNameTextInput.isUserInteractionEnabled = false
+            self.lastNameTextInput.isUserInteractionEnabled = false
+            self.emailTextInput.isUserInteractionEnabled = false
+            self.firstNameTextInput.isUserInteractionEnabled = false
+            self.profilPicker.isUserInteractionEnabled = false
+        }
+       
         //TODO: update avec l'api
     }
     @IBAction func tapToEdit(_ sender: UIButton) {
@@ -68,6 +74,11 @@ class DetailsContactViewController: UIViewController, UIPickerViewDelegate, UIPi
         self.saveButton.isHidden = false
         self.editButton.isHidden = true
         
+        self.firstNameTextInput.isUserInteractionEnabled = true
+        self.lastNameTextInput.isUserInteractionEnabled = true
+        self.emailTextInput.isUserInteractionEnabled = true
+        self.firstNameTextInput.isUserInteractionEnabled = true
+        self.profilPicker.isUserInteractionEnabled = true
         // TODO: change UI
     }
     /*
@@ -89,5 +100,21 @@ class DetailsContactViewController: UIViewController, UIPickerViewDelegate, UIPi
     }
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return 3
+    }
+    func isValidEmail(email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        var validEmail = NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email)
+        if validEmail {
+            validEmail = !email.contains("..")
+        }
+        return validEmail
+    }
+    func isValidPhone(phone: String) -> Bool {
+        let phoneRegex = "^\\d{3}-\\d{3}-\\d{4}$"
+        var validPhone = NSPredicate(format: "SELF MATCHES %@", phoneRegex).evaluate(with: phone)
+        if validPhone {
+            validPhone = !phone.contains("..")
+        }
+        return validPhone
     }
 }
