4399646867cb9ab8ac8ace2729bd8d42058079c0
Amaury <amaurygranvillain17@gmail.com>
2019-01-28 10:05:49 +0100

    Connexion mis en place par APIClient
    connexion + vérif

--- Familink/UI/ViewController/LoginViewController.swift
+++ Familink/UI/ViewController/LoginViewController.swift
@@ -15,19 +15,64 @@ class LoginViewController: UITableViewController {
     @IBOutlet weak var phoneNumberTextLabel: UILabel!
     @IBOutlet weak var passwordTextLabel: UILabel!
     @IBOutlet weak var rememberMeTextLabel: UILabel!
+    var userPhone: String?
+    var userPassword: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     @IBAction func tapOnLogin(_ sender: UIButton) {
-        print("tap login")
-        //Todo: if verif
+        userPhone =  phoneTextInput.text
+        userPassword = passwordTextInput.text
+        print(userPhone ?? "")
+        print(userPassword ?? "")
+        APIClient.instance.login(phone: userPhone ?? "", password: userPassword ?? "", onSucces: { (Result) in
             NotificationCenter.default.post(name: Notification.Name("login"), object: self)
-        // else
+            print(Result)
+        }) { (Error) in
+            print(Error)
+            self.checkError(error: Error)
+        }
     }
 
     @IBAction func tapOnforgetPassword(_ sender: Any) {
     }
     @IBAction func switchRemenberMe(_ sender: Any) {
+    }
+    
+    func checkError(error: String) {
+        switch error {
+        case "User not found":
+            DispatchQueue.main.async {
+                let alert = UIAlertController(
+                    title: "Erreur sur le formulaire",
+                    message: "Le numéro de téléphone est incorrecte",
+                    preferredStyle: .alert
+                )
+                alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+                
+                self.present(alert, animated: true)
+            }
+        case "Password is not valid":
+             DispatchQueue.main.async {
+                let alert = UIAlertController(
+                    title: "Erreur sur le formulaire",
+                    message: "Le mot de passe est incorrecte",
+                    preferredStyle: .alert
+                )
+                alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+                
+                self.present(alert, animated: true)
+            }
+        default :
+            let alert = UIAlertController(
+                title: "Erreur sur le formulaire",
+                message: "Une erreur est survenu, veuillez vérifier vos informations",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+            
+            self.present(alert, animated: true)
+        }
     }
 }
