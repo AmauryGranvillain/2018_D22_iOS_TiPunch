2cb1af69ac0fd8e08ecef1c40e5efa83d38f9bdc
Amaury <amaurygranvillain17@gmail.com>
2019-01-30 16:42:02 +0100

    Ajout de l'expiration du token
    retour sur le login pour se reconnecter si le token est expiré

--- Familink/UI/ViewController/AddContactViewController.swift
+++ Familink/UI/ViewController/AddContactViewController.swift
@@ -74,8 +74,26 @@ class AddContactViewController: UIViewController, UIPickerViewDelegate, UIPicker
                     NotificationCenter.default.post(name: Notification.Name("addContact"), object: self)
                     self.navigationController?.popViewController(animated: true)
                 }
-            }) {error in print(error)}
-            
+            }) {error in
+                if error == "Security token invalid or expired" {
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(
+                            title: "Session expiré",
+                            message: "Veuillez-vous reconnecter pour accèder aux fonctionnalités",
+                            preferredStyle: .alert
+                        )
+                        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (sender) in
+                            let controller = UIStoryboard.init(
+                                name: "Main",
+                                bundle: nil).instantiateViewController(
+                                    withIdentifier: "LoginViewController") as! LoginViewController
+                            
+                            self.navigationController?.show(controller, sender: self)
+                        }))
+                        self.present(alert, animated: true)
+                    }
+                }
+            }
         }
     }
     func alertVerif(message: String, toFocus: UITextField) {
